fe6640577a8a3a0071e1ce30c980dc5e29935926
bahodir373 <zteqwertyuiop672@gmail.com>
2025-02-03 21:05:57 +0500

    it works InshaAllah

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ const duaRouter = require('./routers/dua.router')
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  exposedHeaders: ['Content-Length', 'X-Requested-With'], 
+  credentials: true, 
+  preflightContinue: false,
+  optionsSuccessStatus: 204, 
+}));
+
 app.options('*', cors());
 connectDB()
 
